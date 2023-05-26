eaa6534dd269a48ae19c5d2e66e1b42c275da38f
Cam Monari <54180201+cmonari9@users.noreply.github.com>
2023-05-25 20:03:25 -0400

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -124,10 +124,10 @@
 		</ul>
 	</div>
 	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
+	<span class="copyright">&copy; 2023 - {year} </span>
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
+	<span class="creator">Built and inspired by <a href="https://github.com/nmelhado/league-page">League Page</a> by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
 	<!-- You can remove the donation link (although any donations to help
 	 maintain and enhance League Page would be greatly appreciated!) -->
 	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
